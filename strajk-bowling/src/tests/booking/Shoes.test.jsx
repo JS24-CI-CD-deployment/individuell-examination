c88eb3c4bae6ae63f9fbdff3e13487e2c28ce5c5
robjor88 <robjor88@gmail.com>
2025-12-05 22:53:00 +0100

    Renderar flera skostorleksfält när fler spelare finns

--- strajk-bowling/src/tests/booking/Shoes.test.jsx
+++ strajk-bowling/src/tests/booking/Shoes.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect } from "vitest";
+import { describe, expect, test } from "vitest";
 import Shoes from "../../components/Shoes/Shoes";
 import userEvent from "@testing-library/user-event";
 
@@ -9,6 +9,7 @@ describe("Användaren skall kunna ange skostorlek för varje spelare", () => {
   const removeShoe = vi.fn();
   const shoes = [{ id: "p1" }];
 
+  // AC: ”Användaren ska kunna ange skostorlek för varje spelare.”
   test("Användaren kan ange skostorlek", async () => {
     render(
       <Shoes
@@ -26,6 +27,7 @@ describe("Användaren skall kunna ange skostorlek för varje spelare", () => {
     expect(shoeSize.value).toBe("42");
   });
 
+  // AC: ”Användaren ska kunna ändra skostorlek för varje spelare.”
   test("Användaren ska kunna ändra skostorlek", async () => {
     render(
       <Shoes
@@ -41,5 +43,28 @@ describe("Användaren skall kunna ange skostorlek för varje spelare", () => {
 
     expect(shoeInput.value).toBe("45");
     expect(updateSize).toHaveBeenCalled();
+  });
+
+  //AC: Det ska vara möjligt att välja skostorlek för alla spelare som ingår i bokningen.
+  test("Renderar flera skostorleksfält när flera spelare finns", () => {
+    const players = [{ id: "p1" }, { id: "p2" }, { id: "p3" }, { id: "p4" }];
+
+    render(
+      <Shoes
+        shoes={players}
+        updateSize={updateSize}
+        addShoe={addShoe}
+        removeShoe={removeShoe}
+      />
+    );
+    const shoeInput1 = screen.getByTestId("shoe-input-1");
+    const shoeInput2 = screen.getByTestId("shoe-input-2");
+    const shoeInput3 = screen.getByTestId("shoe-input-3");
+    const shoeInput4 = screen.getByTestId("shoe-input-4");
+
+    expect(shoeInput1).toBeInTheDocument();
+    expect(shoeInput2).toBeInTheDocument();
+    expect(shoeInput3).toBeInTheDocument();
+    expect(shoeInput4).toBeInTheDocument();
   });
 });
